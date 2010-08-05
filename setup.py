d2be3f149361b5e325bfafde01bb96d56a7807fd
palewire <ben.welsh@gmail.com>
2010-08-05 01:44:15 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='greeking',
+      version='0.1',
+      description='Tools for printing filler text, a technique from the days of hot type known as greeking.',
+      author='Ben Welsh',
+      author_email='Benjamin.Welsh@latimes.com',
+      url='http://github.com/palewire/django-greeking',
+      download_url='http://github.com/palewire/django-greeking.git',
+      packages=find_packages(),
+      license='MIT',
+      keywords='greeking pangrams lorem ipsum quotables comments text jabberwocky',
+      classifiers=["Intended Audience :: Developers",
+                   "Intended Audience :: Science/Research",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Topic :: Software Development :: Libraries :: Python Modules"
+                   ],
+     )

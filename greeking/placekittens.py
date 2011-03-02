0abc83660e159f5878059c2ae36988e7663d9b49
palewire <ben.welsh@gmail.com>
2011-03-02 06:41:16 -0800

    Added grayscale images to placekitten function

--- greeking/placekittens.py
+++ greeking/placekittens.py
@@ -2,7 +2,18 @@
 """
 Utility for generating placeholder kitten images
 """
-BASE_URL = 'http://placekitten.com/%(width)s/%(height)s/'
+COLOR_URL = 'http://placekitten.com/%(width)s/%(height)s/'
+GRAY_URL = 'http://placekitten.com/g/%(width)s/%(height)s/'
 
-def get_url(width, height):
-    return BASE_URL % dict(width=width, height=height)
+def get_url(width, height, color=True):
+    """
+    Craft the URL for a placekitten image.
+    
+    By default they are in color. To retrieve a grayscale image, set
+    the color kwarg to False.
+    """
+    d = dict(width=width, height=height)
+    if color:
+        return COLOR_URL % d
+    else:
+        return GRAY_URL % d

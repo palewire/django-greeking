530493f54cc385e3819085ce183587f98a183322
palewire <ben.welsh@gmail.com>
2011-03-02 07:09:48 -0800

    Removed models.py and views.py files we don't use

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='greeking',
-      version='0.11',
+      version='0.121',
       description='Tools for printing filler text, a technique from the days of hot type known as greeking.',
       author='Ben Welsh',
       author_email='Benjamin.Welsh@latimes.com',

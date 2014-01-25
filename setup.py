3a9c4f693d4ff51d1047c7514e1c7efaed3f7313
palewire <ben.welsh@gmail.com>
2014-01-25 11:39:38 -0800

    Renamed verion to make pypi happy. Ugh. I did that stupid thing again.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='greeking',
-    version='0.2',
+    version='0.200',
     description='Django template tools for printing filler text, a \
     technique from the days of hot type known as greeking.',
     author='Ben Welsh',

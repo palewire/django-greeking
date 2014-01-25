9f0c34ed78e79aa873cd6a4c00cdf5bd0123145b
palewire <ben.welsh@gmail.com>
2014-01-25 11:35:11 -0800

    PEP8 on the setup.py file

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
 from setuptools import setup, find_packages
 
-setup(name='greeking',
-      version='0.2',
-      description='Tools for printing filler text, a technique from the days of hot type known as greeking.',
-      author='Ben Welsh',
-      author_email='Benjamin.Welsh@latimes.com',
-      url='http://github.com/palewire/django-greeking',
-      download_url='http://github.com/palewire/django-greeking.git',
-      packages=find_packages(),
-      license='MIT',
-      keywords='greeking pangrams lorem ipsum quotables comments text jabberwocky',
-      classifiers=["Intended Audience :: Developers",
-                   "Intended Audience :: Science/Research",
-                   "License :: OSI Approved :: MIT License",
-                   "Operating System :: OS Independent",
-                   "Programming Language :: Python",
-                   "Topic :: Software Development :: Libraries :: Python Modules"
-                   ],
-      install_requires=[
-        'six==1.5.2'
-      ],
-     )
+setup(
+    name='greeking',
+    version='0.2',
+    description='Django template tools for printing filler text, a \
+    technique from the days of hot type known as greeking.',
+    author='Ben Welsh',
+    author_email='Benjamin.Welsh@latimes.com',
+    url='http://github.com/palewire/django-greeking',
+    download_url='http://github.com/palewire/django-greeking.git',
+    packages=find_packages(),
+    license='MIT',
+    keywords='greeking pangrams lorem ipsum quotables comments \
+    text jabberwocky placekittens fillmurray filler',
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    install_requires=['six==1.5.2']
+)

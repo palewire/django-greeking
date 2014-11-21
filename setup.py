3e24b022c64c9eef95aba6552c9d3e5e31c492a9
palewire <ben.welsh@gmail.com>
2014-11-21 10:08:09 -0800

    Upgraded to setup.py tests

--- setup.py
+++ setup.py
@@ -1,4 +1,40 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from distutils.core import Command
+
+
+class TestCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from django.conf import settings
+        settings.configure(
+            DATABASES={
+                'default': {
+                    'NAME': 'test.db',
+                    'TEST_NAME': 'test.db',
+                    'ENGINE': 'django.db.backends.sqlite3'
+                }
+            },
+            INSTALLED_APPS = (
+                'django.contrib.auth',
+                'django.contrib.contenttypes',
+                'django.contrib.sessions',
+                'django.contrib.staticfiles',
+                'greeking',
+            ),
+        )
+        from django.core.management import call_command
+        import django
+        if django.VERSION[:2] >= (1, 7):
+            django.setup()
+        call_command('test', 'greeking')
+
 
 setup(
     name='greeking',
@@ -9,7 +45,7 @@ setup(
     author_email='ben.welsh@gmail.com',
     url='http://github.com/palewire/django-greeking',
     download_url='http://github.com/palewire/django-greeking.git',
-    packages=find_packages(),
+    packages='greeking',
     license='MIT',
     keywords='greeking pangrams lorem ipsum quotables comments \
     text jabberwocky placekittens fillmurray filler',
@@ -19,7 +55,11 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules"
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    install_requires=['six==1.5.2']
+    install_requires=('six>=1.5.2',),
+    cmdclass={'test': TestCommand}
 )

89232d0175863af5812ffd3908a1bd143f63f78a
palewire <ben.welsh@gmail.com>
2016-07-25 18:03:07 -0700

    v2.1.1

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ class TestCommand(Command):
 
 setup(
     name='greeking',
-    version='2.1.0',
+    version='2.1.1',
     description='Django template tools for printing filler, a \
     technique from the days of hot type known as greeking.',
     author='Ben Welsh',
